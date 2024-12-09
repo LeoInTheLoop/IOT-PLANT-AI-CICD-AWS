316bfdce734caa8c4d1abd3de2fa32a409bab799
kong lingyu <lionyu_kong@outlook.com>
2024-12-09 15:09:32 +0100

    todo
    web page

--- client/src/pages/MachinePage.tsx
+++ client/src/pages/MachinePage.tsx
@@ -1,4 +1,5 @@
 import Navigation from ".././components/Navigation.tsx";
+import { Link } from "react-router-dom";
 
 const MachinePage = () => {
     return (
@@ -6,6 +7,8 @@ const MachinePage = () => {
             <Navigation />
             <h1>Welcome to the New Page</h1>
             <p>This is a new page in your application.</p>
+            <p> use this page to should overal machine status </p>
+            <p> and this link should linechart of one machine prarms  </p><Link to="/" className="text-purple-800 underline"></Link>
         </div>
     );
 };
