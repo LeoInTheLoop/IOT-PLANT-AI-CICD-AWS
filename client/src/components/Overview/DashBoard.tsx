93ef585ea9a81080906455ee409c246cc6389265
kong lingyu <lionyu_kong@outlook.com>
2025-08-26 11:32:12 +0200

    test action

--- client/src/components/Overview/DashBoard.tsx
+++ client/src/components/Overview/DashBoard.tsx
@@ -5,7 +5,7 @@ const DashBoard = () => {
   return (
     <div className="bg-[#fffefb] rounded-lg pb-10 shadow h-full min-h-screen">
       {" "}
-      <TopBar title="Machine List" />
+      <TopBar title="Machine List ahaha" />
       <MachineList />
     </div>
   );
